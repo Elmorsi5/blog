1e016f81db7660f8a78576d1441a881400f3eef5
Mohamed Elmorsi <61989626+Elmorsi5@users.noreply.github.com>
2023-08-09 16:45:03 +0300

    Adding a many to one relatinship User and Posts

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Post(models.model):
 
     class Status(models.TextChoices):
-        # DRAFT: use as a variabl in the application, Df: in the database, Draft: human readable format
+        # DRAFT(names): use as a variabl in the application, Df(vallues): in the database, Draft(labels):human readable format
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
@@ -19,6 +20,10 @@ class Post(models.model):
     # auto_now save automatically when we save the object , save == updated 
     updated = models.DateTimeField(auto_now = True) 
     status = models.CharField(max_length=2,choices=Status.choices,default=Status.DRAFT)
+    # on_delete: defint the behavior when the referenced object is deleted ( the user )
+    # Related_name : We use it to access the posts of a specific user using this format:(user.blog_posts) 
+
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
     class Meta:
         ordering = ['-publish']
         index = [
