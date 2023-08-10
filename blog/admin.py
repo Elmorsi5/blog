480ef23e399f4a0998d5427541947f6de701b961
Mohamed Elmorsi <61989626+Elmorsi5@users.noreply.github.com>
2023-08-10 10:02:01 +0300

    Add more options in customizing the Django-admin site

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,11 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [ 'title','slug','author','publish','status']
-    
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    # Note: The value of the 'prepopulated_fields' must be a list or tuple 
+    prepopulated_fields = {'slug': ('title',),} 
+    # raw_id_fields: the author field is now displayed with a lookup widget, which can be much better than a drop-down select input when you have thousands of users 
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
