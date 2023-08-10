274c93990d21a6991487488b18b17deb1b2bc5fa
Mohamed Elmorsi <61989626+Elmorsi5@users.noreply.github.com>
2023-08-10 12:01:42 +0300

    create a post_list view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.http import Http404
 # Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,'blog/post/list.html',{'posts':posts})
+
+
