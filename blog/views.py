4181e388b67ff531e2c1f15d032ffdb563fc7d50
Mohamed Elmorsi <61989626+Elmorsi5@users.noreply.github.com>
2023-08-12 13:59:59 +0300

    Send Email and Comment system

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post,Comment
 from django.http import Http404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm,CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 # Create your views here.
 
 
@@ -41,20 +43,47 @@ def post_detail(request,year,month,day,post):
                              publish__year = year,
                              publish__month = month,
                              publish__day = day)
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+    return render(request,'blog/post/detail.html',{'post': post,'comments': comments,'form': form})
     
-    return render(request,'blog/post/detail.html', {'post':post})
-
 
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             #Form fields passed validation
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url()) 
+            subject = f"{cd['name']} recommends you read " \
+                f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'mohamedelmorse5@gmail.com',
+                      [cd['to']])
+            sent = True
             # ... send email
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post,'form': form})
+    return render(request, 'blog/post/share.html', {'post': post,'form': form,'sent': sent})
+
+
+@require_POST
+def post_comment(request,post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(request, 'blog/post/comment.html',{'post': post,'form': form,'comment': comment})
